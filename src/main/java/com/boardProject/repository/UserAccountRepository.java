8609d4058bc41ad420949c0638e4e496876a3e91
joosang425 <dkrehd456@naver.com>
2023-10-30 20:24:16 +0900

    Revert "#17 - 회원 계정 도메인 구현 및 erd 반영"
    This reverts commit f3a9c869109987587feceaf2d01242279e2cd440.

--- src/main/java/com/boardProject/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.boardProject.repository;
-
-import com.boardProject.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-}
