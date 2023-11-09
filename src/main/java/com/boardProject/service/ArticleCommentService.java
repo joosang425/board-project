0ff581dd382398ea9a6983881b54c4cca85ae075
joosang425 <dkrehd456@naver.com>
2023-11-09 20:25:39 +0900

    #46 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 구현

--- src/main/java/com/boardProject/service/ArticleCommentService.java
+++ src/main/java/com/boardProject/service/ArticleCommentService.java
@@ -1,9 +1,12 @@
 package com.boardProject.service;
 
+import com.boardProject.domain.Article;
 import com.boardProject.domain.ArticleComment;
+import com.boardProject.domain.UserAccount;
 import com.boardProject.dto.ArticleCommentDto;
 import com.boardProject.repository.ArticleCommentRepository;
 import com.boardProject.repository.ArticleRepository;
+import com.boardProject.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,7 @@ public class ArticleCommentService {
 
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
@@ -31,9 +35,11 @@ public class ArticleCommentService {
 
     public void saveArticleComment(ArticleCommentDto dto) {
         try {
-            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+            Article article = articleRepository.getReferenceById(dto.articleId());
+            UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
+            articleCommentRepository.save(dto.toEntity(article, userAccount));
         } catch (EntityNotFoundException e) {
-            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+            log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
         }
     }
 
